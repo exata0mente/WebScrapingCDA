7bcf65dcd208f966a5ee16b656c9840150dff7e8
exata0mente <exata0mente@gmail.com>
2018-11-28 20:07:18 -0200

    Retira linhas de form devida a não utilidade (no momento)

--- Obj/etapa_busca.r
+++ Obj/etapa_busca.r
@@ -1,16 +1,14 @@
+# -[ ] Validar se há obras que não possuem campos iguais a todas as outras
+
+rm(list=ls())
+
 library(httr)
 library(rvest)
 
-url_acesso <- "https://www.catalogodasartes.com.br/acesso"
+## Etapa de Sessão
+url_acesso <- "https://www.catalogodasartes.com.br/inicio"
 
 sessao <- html_session(url_acesso)
 
-login <- sessao %>%
-  html_node("form") %>%
-  html_form() %>%
-  set_values('cliente_email' = 'profantonioalbuquerque@gmail.com', 'cliente_senha' = 'flamengo')
-
-login$url <- url
-
-sessao_logada <- sessao %>%
-  submit_form(login)
+listaObrasHome <- sessao %>%
+  html_nodes(".card-reveal")
