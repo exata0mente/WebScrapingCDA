86e04e90ed523f1d30d5ed4f89fa986302f428e0
exata0mente <exata0mente@gmail.com>
2018-11-29 20:53:37 -0200

    Adiciona tratamento de obras com colunas a mais

--- Obj/etapa_busca.r
+++ Obj/etapa_busca.r
@@ -1,14 +1,58 @@
-# -[ ] Validar se há obras que não possuem campos iguais a todas as outras
-
 rm(list=ls())
 
 library(httr)
 library(rvest)
+library(jsonlite)
+library(dplyr)
+
 
 ## Etapa de Sessão
 url_acesso <- "https://www.catalogodasartes.com.br/inicio"
 
 sessao <- html_session(url_acesso)
 
-listaObrasHome <- sessao %>%
-  html_nodes(".card-reveal")
+## Carrega os produtos da home page
+listaObrasHome <-sessao %>%
+  html_nodes(".card.produto")
+
+c_principal <- c("Tipo",
+                 "Artista",
+                 "Título",
+                 "Técnica",
+                 "Dimensões",
+                 "Descrição",
+                 "Avaliação da Obra",
+                 "Data da Pesquisa")
+
+## Define variáveis
+df_principal <- data.frame(matrix(nrow = 0, ncol = length(c_principal)))
+names(df_principal) <- c_principal
+n <- length(listaObrasHome)
+
+# Realiza a mineração retornando uma lista
+for(i in 1:n){
+  
+  c <- listaObrasHome[i] %>%
+    html_nodes(".detalhes-titulo") %>%
+    html_text()
+  c <- combine("Tipo", c)
+  
+  r <- listaObrasHome[i] %>%
+    html_nodes(".card-content span span:last-child, .detalhes-texto, .detalhes-texto-justificado") %>%
+    html_text()
+
+  m <- length(c)
+  
+  for(j in 1:m){
+    if(!any(c_principal == c[j])){
+      c_principal <- combine(c_principal, c[j])
+      df_principal <- mutate(df_principal, nc = NA)
+      names(df_principal) <- c_principal
+    }
+    pos <- which(c_principal == c[j])
+    df_principal[i,pos] <- r[j]
+  }
+    
+}
+
+write.csv(df_principal, "20181129_listaObrasHome.csv")
---
+++ README.md
@@ -0,0 +1,8 @@
+# Referências úteis
+
+flukout.github.io - Este site dá uma boa ajuda para entender o CSS Selector, muito utilizado no `html_node`, através de um jogo.
+
+# Observações sobre algumas funções.
+
+* `rvest::html_nodes(x, css, xpath)` - Traz todos os nós que correspondem ao CSS selector ou ao XPath. Exemplo.
+    `html_nodes(tmp, "span.detalhes-texto")`
