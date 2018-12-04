1bd34c19504adfc908dc5fa4cdaf7bb572d05c64
exata0mente <exata0mente@gmail.com>
2018-12-04 21:47:39 -0200

    Rotina pronta para uso.

--- Funcs/main.R
+++ Funcs/main.R
@@ -3,53 +3,39 @@
 # Descrição: Este script (ou conjunto de funções) realiza uma mineração em um site de obra de artes
 #            extraindo dados relevantes para enriquecimento de uma base
 
-# Mapa de prefixos dos objetos:
-# pg* - Página
-# s*  - Sessão
-# o*  - Objeto
-# u*  - Url
-# e*  - Elementos
-
-########## APAGAR ##########
-oUsuario <- "profantonioalbuquerque@gmail.com"
-oSenha <- "flamengo"
-
-rm(list=ls())
+rm(list=ls()) 
 system('docker run -d -p 4445:4444 selenium/standalone-firefox')
 
 # Carregamento de pacotes
 library(RSelenium)
+library(rvest)
+library(httr)
 library(dplyr)
-source("funcoes.R")
+library(jsonlite)
+source("../Funcs/funcoesCDA.R")
 
 # Definição de url base
 uBase <- "http://www.catalogodasartes.com.br/"
 uLogin <-paste0(uBase, "acesso/")
 
 # Etapa de Sessão
-remDr <- remoteDriver(remoteServerAddr = "localhost", 
+navegDriver <- remoteDriver(remoteServerAddr = "localhost", 
                       port = 4445L, 
                       browserName = "firefox")
-remDr$open()
+navegDriver$open()
 
-loginCDA(remDr = remDr,
+loginCDA(remDr = navegDriver,
          usuario = oUsuario,
          senha = oSenha,
          url_login = uLogin)
 
+navegDriver$navigate(paste0(uBase, "inicio/"))
 
 # Etapa de coleta de dados das obras
 
-oCabecalho <- c("Tipo",
-                 "Artista",
-                 "Título",
-                 "Técnica",
-                 "Dimensões",
-                 "Descrição",
-                 "Avaliação da Obra",
-                 "Data da Pesquisa")
+pgFonte <- pesquisaTextoLivreCDA(navegDriver, "Abraham Palatnik")
 
-dfObras <- data.frame(matrix(nrow = 0, ncol = length(oCabecalho)))
+# Etapa de tratamento dos dados
 
-#usar o lapply
+listDfs <- listDfCardsCDA(pgFonte) ## Este arquivo está compatível com o formato JSON
 
