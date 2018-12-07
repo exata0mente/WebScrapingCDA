d0dfe2d031c5a7c26eb79719a49d5d144dd0a0e1
exata0mente <exata0mente@gmail.com>
2018-12-07 00:21:33 -0200

    Cria função que converte uma lista de dfs de diferentes tamanhos em uma lista de dfs de mesmo tamanho.

--- R/funcoesCDA.R
+++ R/funcoesCDA.R
@@ -220,3 +220,29 @@ minerarObrasFull <- function(remDr = NULL, expressao = character(),pagina = NULL
   # Carrego
 }
 
+mesclaObrasDf <- function(x){
+  cabecalhoPadrao <- readRDS(file = "Cabecalhos.RDS")
+  df_principal <- data.frame(matrix(nrow = 0, ncol = length(cabecalhoPadrao)))
+  names(df_principal) <- cabecalhoPadrao
+  tam <- length(x)
+  cabecalhoTmp <- names(x)
+  
+  for(i in 1:tam){
+    if(!any(cabecalhoTmp[i] == cabecalhoPadrao)){
+      cabecalhoPadrao <- combine(cabecalhoPadrao,cabecalhoTmp[i])
+      df_principal <- mutate(df_principal, nc = "N/D")
+      names(df_principal) <- cabecalhoPadrao
+    }
+    pos <- which(cabecalhoPadrao == cabecalhoTmp[i])
+    df_principal[1,pos] <- x[i]
+  }
+  saveRDS(cabecalhoPadrao, file = "Cabecalhos.RDS")
+  df_principal
+}
+
+listaObras2csv <- function(arquivosObras = c()){
+  
+  
+  
+  
+}
